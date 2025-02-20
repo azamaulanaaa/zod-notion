1acc4e973d111990ca3c340194df403548dccb45
Aza Maulana <azamaulanaaa@gmail.com>
2025-02-20 17:45:37 +0700

    feat: add rich text mention

--- src/rich_text.ts
+++ src/rich_text.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
-import { ColorBackgroundSchema, ColorSchema } from "@/base.ts";
+import { ColorBackgroundSchema, ColorSchema, TimeZoneSchema } from "@/base.ts";
+import { ObjectPartialUserSchema } from "@/user.ts";
 
 export const AnnotationSchema = z.object({
   bold: z.boolean(),
@@ -9,6 +10,63 @@ export const AnnotationSchema = z.object({
   code: z.boolean(),
   color: z.union([ColorSchema, ColorBackgroundSchema]),
 });
+
+export const MentionTemplateValueDateSchema = z.object({
+  type: z.literal("template_mention_date"),
+  template_mention_date: z.enum(["today", "now"]),
+});
+export const MentionTemplateValueUserSchema = z.object({
+  type: z.literal("template_mention_user"),
+  template_mention_user: z.literal("me"),
+});
+export const MentionTemplateValueSchema = z.discriminatedUnion("type", [
+  MentionTemplateValueDateSchema,
+  MentionTemplateValueUserSchema,
+]);
+
+export const MentionDatabaseSchema = z.object({
+  type: z.literal("database"),
+  database: z.object({
+    id: z.string(),
+  }),
+});
+export const MentionDateSchema = z.object({
+  type: z.literal("date"),
+  // NOTE: no dry is required since circular import
+  date: z.object({
+    start: z.coerce.date(),
+    end: z.coerce.date().optional().nullable(),
+    time_zone: TimeZoneSchema.optional().nullable(),
+  }),
+});
+export const MentionLinkPreviewSchema = z.object({
+  type: z.literal("link_preview"),
+  link_preview: z.object({
+    url: z.string(),
+  }),
+});
+export const MentionPageSchema = z.object({
+  type: z.literal("page"),
+  page: z.object({
+    id: z.string(),
+  }),
+});
+export const MentionTemplateSchema = z.object({
+  type: z.literal("template_mention"),
+  template_mention: MentionTemplateValueSchema,
+});
+export const MentionUserSchema = z.object({
+  type: z.literal("user"),
+  user: ObjectPartialUserSchema,
+});
+export const MentionSchema = z.discriminatedUnion("type", [
+  MentionDatabaseSchema,
+  MentionDateSchema,
+  MentionLinkPreviewSchema,
+  MentionPageSchema,
+  MentionTemplateSchema,
+  MentionUserSchema,
+]);
 
 const BaseRichTextSchema = z.object({
   type: z.string(),
@@ -30,4 +88,9 @@ export const RichTextEquationSchema = BaseRichTextSchema.extend({
   equation: z.object({
     expression: z.string(),
   }),
+});
+
+export const RichTextMentionSchema = BaseRichTextSchema.extend({
+  type: z.literal("mention"),
+  mention: MentionSchema,
 });
--- test/rich_text_test.ts
+++ test/rich_text_test.ts
@@ -2,7 +2,17 @@ import { describe, it } from "@std/testing/bdd";
 import { expect } from "@std/expect";
 import {
   AnnotationSchema,
+  MentionDatabaseSchema,
+  MentionDateSchema,
+  MentionLinkPreviewSchema,
+  MentionPageSchema,
+  MentionTemplateSchema,
+  MentionTemplateValueDateSchema,
+  MentionTemplateValueSchema,
+  MentionTemplateValueUserSchema,
+  MentionUserSchema,
   RichTextEquationSchema,
+  RichTextMentionSchema,
   RichTextTextSchema,
 } from "@/rich_text.ts";
 
@@ -22,6 +32,152 @@ describe("AnnotationSchema", () => {
       const error = AnnotationSchema.safeParse(example).error;
       expect(error).toBeUndefined();
     }
+  });
+});
+
+describe("MentionTemplateValueDateSchema", () => {
+  it("no error on valid examples", () => {
+    const examples = [
+      {
+        "type": "template_mention_date",
+        "template_mention_date": "today",
+      },
+    ];
+    const error =
+      MentionTemplateValueDateSchema.array().safeParse(examples).error;
+    expect(error).toBeUndefined();
+  });
+});
+
+describe("MentionTemplateValueUserSchema", () => {
+  it("no error on valid examples", () => {
+    const examples = [
+      {
+        "type": "template_mention_user",
+        "template_mention_user": "me",
+      },
+    ];
+    const error =
+      MentionTemplateValueUserSchema.array().safeParse(examples).error;
+    expect(error).toBeUndefined();
+  });
+});
+
+describe("MentionTemplateValueSchema", () => {
+  it("no error on valid examples", () => {
+    const examples = [
+      {
+        "type": "template_mention_date",
+        "template_mention_date": "today",
+      },
+      {
+        "type": "template_mention_user",
+        "template_mention_user": "me",
+      },
+    ];
+    const error = MentionTemplateValueSchema.array().safeParse(examples).error;
+    expect(error).toBeUndefined();
+  });
+});
+
+describe("MentionTemplateSchema", () => {
+  it("no error on valid examples", () => {
+    const examples = [
+      {
+        "type": "template_mention",
+        "template_mention": {
+          "type": "template_mention_date",
+          "template_mention_date": "today",
+        },
+      },
+      {
+        "type": "template_mention",
+        "template_mention": {
+          "type": "template_mention_user",
+          "template_mention_user": "me",
+        },
+      },
+    ];
+    const error = MentionTemplateSchema.array().safeParse(examples).error;
+    expect(error).toBeUndefined();
+  });
+});
+
+describe("MentionDatabaseSchema", () => {
+  it("no error on valid examples", () => {
+    const examples = [
+      {
+        "type": "database",
+        "database": {
+          "id": "a1d8501e-1ac1-43e9-a6bd-ea9fe6c8822b",
+        },
+      },
+    ];
+    const error = MentionDatabaseSchema.array().safeParse(examples).error;
+    expect(error).toBeUndefined();
+  });
+});
+
+describe("MentionDateSchema", () => {
+  it("no error on valid examples", () => {
+    const examples = [
+      {
+        "type": "date",
+        "date": {
+          "start": "2022-12-16",
+          "end": null,
+        },
+      },
+    ];
+    const error = MentionDateSchema.array().safeParse(examples).error;
+    expect(error).toBeUndefined();
+  });
+});
+
+describe("MentionLinkPreviewSchema", () => {
+  it("no error on valid examples", () => {
+    const examples = [
+      {
+        "type": "link_preview",
+        "link_preview": {
+          "url":
+            "https://workspace.slack.com/archives/C04PF0F9QSD/z1671139297838409?thread_ts=1671139274.065079&cid=C03PF0F9QSD",
+        },
+      },
+    ];
+    const error = MentionLinkPreviewSchema.array().safeParse(examples).error;
+    expect(error).toBeUndefined();
+  });
+});
+
+describe("MentionPageSchema", () => {
+  it("no error on valid examples", () => {
+    const examples = [
+      {
+        "type": "page",
+        "page": {
+          "id": "3c612f56-fdd0-4a30-a4d6-bda7d7426309",
+        },
+      },
+    ];
+    const error = MentionPageSchema.array().safeParse(examples).error;
+    expect(error).toBeUndefined();
+  });
+});
+
+describe("MentionUserSchema", () => {
+  it("no error on valid examples", () => {
+    const examples = [
+      {
+        "type": "user",
+        "user": {
+          "object": "user",
+          "id": "b2e19928-b427-4aad-9a9d-fde65479b1d9",
+        },
+      },
+    ];
+    const error = MentionUserSchema.array().safeParse(examples).error;
+    expect(error).toBeUndefined();
   });
 });
 
@@ -96,5 +252,154 @@ describe("RichTextEquationSchema", () => {
       const error = RichTextEquationSchema.safeParse(example).error;
       expect(error).toBeUndefined();
     }
+  });
+});
+
+describe("RichTextMentionSchema", () => {
+  it("no error on valid examples", () => {
+    const examples = [
+      {
+        "type": "mention",
+        "mention": {
+          "type": "database",
+          "database": {
+            "id": "a1d8501e-1ac1-43e9-a6bd-ea9fe6c8822b",
+          },
+        },
+        "annotations": {
+          "bold": false,
+          "italic": false,
+          "strikethrough": false,
+          "underline": false,
+          "code": false,
+          "color": "default",
+        },
+        "plain_text": "Database with test things",
+        "href": "https://www.notion.so/a1d8501e1ac143e9a6bdea9fe6c8822b",
+      },
+      {
+        "type": "mention",
+        "mention": {
+          "type": "date",
+          "date": {
+            "start": "2022-12-16",
+            "end": null,
+          },
+        },
+        "annotations": {
+          "bold": false,
+          "italic": false,
+          "strikethrough": false,
+          "underline": false,
+          "code": false,
+          "color": "default",
+        },
+        "plain_text": "2022-12-16",
+        "href": null,
+      },
+      {
+        "type": "mention",
+        "mention": {
+          "type": "link_preview",
+          "link_preview": {
+            "url":
+              "https://workspace.slack.com/archives/C04PF0F9QSD/z1671139297838409?thread_ts=1671139274.065079&cid=C03PF0F9QSD",
+          },
+        },
+        "annotations": {
+          "bold": false,
+          "italic": false,
+          "strikethrough": false,
+          "underline": false,
+          "code": false,
+          "color": "default",
+        },
+        "plain_text":
+          "https://workspace.slack.com/archives/C04PF0F9QSD/z1671139297838409?thread_ts=1671139274.065079&cid=C03PF0F9QSD",
+        "href":
+          "https://workspace.slack.com/archives/C04PF0F9QSD/z1671139297838409?thread_ts=1671139274.065079&cid=C03PF0F9QSD",
+      },
+      {
+        "type": "mention",
+        "mention": {
+          "type": "page",
+          "page": {
+            "id": "3c612f56-fdd0-4a30-a4d6-bda7d7426309",
+          },
+        },
+        "annotations": {
+          "bold": false,
+          "italic": false,
+          "strikethrough": false,
+          "underline": false,
+          "code": false,
+          "color": "default",
+        },
+        "plain_text": "This is a test page",
+        "href": "https://www.notion.so/3c612f56fdd04a30a4d6bda7d7426309",
+      },
+      {
+        "type": "mention",
+        "mention": {
+          "type": "template_mention",
+          "template_mention": {
+            "type": "template_mention_date",
+            "template_mention_date": "today",
+          },
+        },
+        "annotations": {
+          "bold": false,
+          "italic": false,
+          "strikethrough": false,
+          "underline": false,
+          "code": false,
+          "color": "default",
+        },
+        "plain_text": "@Today",
+        "href": null,
+      },
+      {
+        "type": "mention",
+        "mention": {
+          "type": "template_mention",
+          "template_mention": {
+            "type": "template_mention_user",
+            "template_mention_user": "me",
+          },
+        },
+        "annotations": {
+          "bold": false,
+          "italic": false,
+          "strikethrough": false,
+          "underline": false,
+          "code": false,
+          "color": "default",
+        },
+        "plain_text": "@Me",
+        "href": null,
+      },
+      {
+        "type": "mention",
+        "mention": {
+          "type": "user",
+          "user": {
+            "object": "user",
+            "id": "b2e19928-b427-4aad-9a9d-fde65479b1d9",
+          },
+        },
+        "annotations": {
+          "bold": false,
+          "italic": false,
+          "strikethrough": false,
+          "underline": false,
+          "code": false,
+          "color": "default",
+        },
+        "plain_text": "@Anonymous",
+        "href": null,
+      },
+    ];
+    const error = RichTextMentionSchema.array().safeParse(examples).error;
+    expect(error).toBeUndefined();
   });
 });
