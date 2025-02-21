0696096cd421ece42da753a48bb1c502210397b5
Aza Maulana <azamaulanaaa@gmail.com>
2025-02-21 15:32:03 +0700

    test: update example of property value for date
    remplace previous example with other examples from the docs. the docs is sucks so i add url for reference

--- test/property_value_test.ts
+++ test/property_value_test.ts
@@ -59,6 +59,30 @@ describe("PropertyValueCreatedTimeSchema", () => {
   });
 });
 
+// NOTE: Reference https://developers.notion.com/reference/page-property-values#date
+describe("PropertyValueDateSchema", () => {
+  it("no error on valid examples", () => {
+    const examples = [
+      {
+        "date": {
+          "start": "2023-02-23",
+        },
+      },
+      {
+        "id": "M%3BBw",
+        "type": "date",
+        "date": {
+          "start": "2023-02-07",
+          "end": null,
+          "time_zone": null,
+        },
+      },
+    ];
+    const error = PropertyValueDateSchema.array().safeParse(examples).error;
+    expect(error).toBeUndefined();
+  });
+});
+
 // NOTE: Reference https://developers.notion.com/reference/page-property-values#title
 describe("PropertyValueTitleSchema", () => {
   it("no error on valid examples", () => {
@@ -89,33 +113,5 @@ describe("PropertyValueTitleSchema", () => {
     ];
     const error = PropertyValueTitleSchema.array().safeParse(examples).error;
     expect(error).toBeUndefined();
-  });
-});
-
-describe("PropertyValueDateSchema", () => {
-  it("no error on valid examples", () => {
-    const examples = [
-      {
-        "date": {
-          "start": "2021-05-11T11:00:00.000-04:00",
-        },
-      },
-      {
-        "date": {
-          "start": "2021-04-26",
-          "end": "2021-05-07",
-        },
-      },
-      {
-        "date": {
-          "start": "2020-12-08T12:00:00Z",
-          "time_zone": "America/New_York",
-        },
-      },
-    ];
-    for (const example of examples) {
-      const error = PropertyValueDateSchema.safeParse(example).error;
-      expect(error).toBeUndefined();
-    }
   });
 });
