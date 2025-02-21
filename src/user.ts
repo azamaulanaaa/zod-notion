d9c2fbe5b8da640aa6cb4770e2ad64a249a4ca2f
Aza Maulana <azamaulanaaa@gmail.com>
2025-02-21 17:05:50 +0700

    feat: add object user
    - add object full user
    - add object full user person
    - add object full user bot

--- src/user.ts
+++ src/user.ts
@@ -4,3 +4,43 @@ import { ObjectBaseSchema } from "@/base.ts";
 export const ObjectPartialUserSchema = ObjectBaseSchema.extend({
   object: z.literal("user"),
 });
+
+const ObjectFullUserBaseSchema = ObjectPartialUserSchema.extend({
+  type: z.string(),
+  name: z.string(),
+  avatar_url: z.string().nullable(),
+});
+export const ObjectFullUserPersonSchema = ObjectFullUserBaseSchema.extend({
+  type: z.literal("person").optional().default("person"),
+  person: z.object({
+    email: z.string().email(),
+  }),
+});
+export const ObjectFullUserBotSchema = ObjectFullUserBaseSchema.extend({
+  type: z.literal("bot").optional().default("bot"),
+  bot: z.union([
+    z.object({
+      owner: z.object({
+        type: z.literal("workspace"),
+        workspace: z.literal(true),
+      }),
+      workspace_name: z.string(),
+    }),
+    z.object({
+      owner: z.object({
+        type: z.literal("user"),
+        user: z.union([ObjectFullUserPersonSchema, ObjectPartialUserSchema]),
+      }),
+      workspace_name: z.literal(null),
+    }),
+  ]),
+});
+export const ObjectFullUserSchema = z.union([
+  ObjectFullUserPersonSchema,
+  ObjectFullUserBotSchema,
+]);
+
+export const ObjectUserSchema = z.union([
+  ObjectFullUserSchema,
+  ObjectPartialUserSchema,
+]);
--- test/user_test.ts
+++ test/user_test.ts
@@ -1,6 +1,11 @@
 import { describe, it } from "@std/testing/bdd";
 import { expect } from "@std/expect";
-import { ObjectPartialUserSchema } from "@/user.ts";
+import {
+  ObjectFullUserBotSchema,
+  ObjectFullUserPersonSchema,
+  ObjectFullUserSchema,
+  ObjectPartialUserSchema,
+} from "@/user.ts";
 
 describe("ObjectPartialUserSchema", () => {
   it("have `object` property", () => {
@@ -31,5 +36,76 @@ describe("ObjectPartialUserSchema", () => {
       id: "59833787-2cf9-4fdf-8782-e53db20768a5",
     }).success;
     expect(success).toBeTruthy();
+  });
+});
+
+// NOTE: Reference https://developers.notion.com/reference/user
+describe("ObjectFullUserPersonSchema", () => {
+  it("no error on valid examples", () => {
+    const examples = [{
+      object: "user",
+      id: "e79a0b74-3aba-4149-9f74-0bb5791a6ee6",
+      type: "person",
+      name: "Avocado Lovelace",
+      avatar_url:
+        "https://secure.notion-static.com/e6a352a8-8381-44d0-a1dc-9ed80e62b53d.jpg",
+      person: { email: "avo@example.org" },
+    }];
+    const error = ObjectFullUserPersonSchema.array().safeParse(examples).error;
+    expect(error).toBeUndefined();
+  });
+});
+
+// NOTE: Reference https://developers.notion.com/reference/user
+describe("ObjectFullUserBotSchema", () => {
+  it("no error on valid examples", () => {
+    const examples = [{
+      object: "user",
+      id: "e79a0b74-3aba-4149-9f74-0bb5791a6ee6",
+      type: "bot",
+      name: "Avocado Lovelace",
+      avatar_url:
+        "https://secure.notion-static.com/e6a352a8-8381-44d0-a1dc-9ed80e62b53d.jpg",
+      bot: {
+        owner: {
+          type: "workspace",
+          workspace: true,
+        },
+        workspace_name: "Ada Lovelace's Notion",
+      },
+    }];
+    const error = ObjectFullUserBotSchema.array().safeParse(examples).error;
+    expect(error).toBeUndefined();
+  });
+});
+
+// NOTE: Reference https://developers.notion.com/reference/user
+describe("ObjectFullUserSchema", () => {
+  it("no error on valid examples", () => {
+    const examples = [{
+      object: "user",
+      id: "e79a0b74-3aba-4149-9f74-0bb5791a6ee6",
+      type: "person",
+      name: "Avocado Lovelace",
+      avatar_url:
+        "https://secure.notion-static.com/e6a352a8-8381-44d0-a1dc-9ed80e62b53d.jpg",
+      person: { email: "avo@example.org" },
+    }, {
+      object: "user",
+      id: "e79a0b74-3aba-4149-9f74-0bb5791a6ee6",
+      type: "bot",
+      name: "Avocado Lovelace",
+      avatar_url:
+        "https://secure.notion-static.com/e6a352a8-8381-44d0-a1dc-9ed80e62b53d.jpg",
+      bot: {
+        owner: {
+          type: "workspace",
+          workspace: true,
+        },
+        workspace_name: "Ada Lovelace's Notion",
+      },
+    }];
+    const error = ObjectFullUserSchema.array().safeParse(examples).error;
+    expect(error).toBeUndefined();
   });
 });
